570f11f533b100ea8ab3e08f74c566beb23637eb
Дмитрий <dvr07@yandex.ru>
2020-10-27 19:36:35 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <img src='https://cdn.freelance.ru/img/portfolio/pics/00/36/9A/3578418.jpg?mt=fe82179e' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+      <img src='https://i.artfile.ru/1920x1200_454642_[www.ArtFile.ru].jpg' />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+}
+
+export default Profile;
