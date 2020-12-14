b1ec074303ab661fb4854f3ee97fe212fb1d34b4
Дмитрий <dvr07@yandex.ru>
2020-12-14 22:30:43 +0300

    Adding components of container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import { Route } from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 const App = (props) => {
 
@@ -20,19 +21,16 @@ const App = (props) => {
                 <
                 Route path = '/dialogs'
                 render = {
-                    () => < Dialogs store = { props.store }
+                    () => < DialogsContainer store = { props.store }
                     /> } / >
 
                     <
                     Route path = '/profile'
                     render = {
-                        () => < Profile
-                        profilePage = { props.state.profilePage }
-                        dispatch = { props.dispatch }
+                        () => < Profile store = { props.store }
                         /> } / >
                         <
-                        /div> <
-                        /div> 
+                        /div> </div >
                     );
                 }
 
