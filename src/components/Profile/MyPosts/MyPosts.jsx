860cb8309d84bc17435842b7cc4bdf16424fc605
Дмитрий <dvr07@yandex.ru>
2020-12-14 22:37:07 +0300

    Addind components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import {addPostActionCreator, updateNewPostTextActionCreator} from '../../../redux/profile-reducer';
-
 
 const MyPosts = (props) =>{
     let postsElements = 
@@ -10,13 +8,12 @@ const MyPosts = (props) =>{
 
      let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
     let onPostChange = () =>{
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
     return(
@@ -28,7 +25,7 @@ const MyPosts = (props) =>{
             value={props.newPostText}/>
             </div>
             <div>
-            <button onClick={addPost}>Add post</button>
+            <button onClick={onAddPost}>Add post</button>
             </div>
         </div>
         <div className={s.posts}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
+import MyPostsContainer from './MyPosts/MyPostsContainer';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 // import Profile from './Profile/Profile';
 
@@ -8,9 +9,7 @@ const Profile = (props) =>{
     return(
         <div>
             <ProfileInfo />
-            <MyPosts posts={props.profilePage.posts} 
-            newPostText={props.profilePage.newPostText}
-            dispatch={props.dispatch}/>
+            <MyPostsContainer store={props.store} />
             </div>
             );
 }
