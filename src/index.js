aebec825eb43c5359643072c9e9dc7d6175ca9d8
Дмитрий <dvr07@yandex.ru>
2020-12-16 22:28:38 +0300

    Adding pure functions

--- src/index.js
+++ src/index.js
@@ -9,22 +9,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 let rerenderEntireTree = (state) => {
-        ReactDOM.render( <
-            BrowserRouter >
-            <
-            Provider store = { store } >
-            <
-            App / >
-            <
-            /Provider> <
-            /BrowserRouter>, document.getElementById('root'));
-        }
+    ReactDOM.render( <
+        BrowserRouter >
+        <
+        Provider store = { store } >
+        <
+        App / >
+        <
+        /Provider> </BrowserRouter > , document.getElementById('root'));
+}
 
-        rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
-        store.subscribe(() => {
-            rerenderEntireTree();
-        });
-
-
-        serviceWorker.unregister();
+serviceWorker.unregister();
