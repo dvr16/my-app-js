658f332254171635325518fbcdb92fdf730a8c0b
Дмитрий <dvr07@yandex.ru>
2021-02-16 18:38:39 +0300

    adding mapDispatchToProps

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { followAC, setCurrentPageAC, setUsersAC, unfollowAC, setUsersTotalCountAC, toggleIsFetchingAC } from '../../redux/users-reducer';
+import { follow, setCurrentPage, setUsers, unfollow, setTotalUsersCount, toggleIsFetching } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
 //import preloader from '../../assets/images/preloader.png';
@@ -52,27 +52,33 @@ let mapStateToProps = (state) =>{
     }
 }
 
-let mapDispatchToProps = (dispatch) =>{
-    return {
-        follow: (userId) =>{
-            dispatch(followAC(userId));
-        },
-        unfollow: (userId) =>{
-            dispatch(unfollowAC(userId));
-        },
-        setUsers: (users) =>{
-            dispatch(setUsersAC(users));
-        },
-        setCurrentPage: (pageNumber) => {
-            dispatch(setCurrentPageAC(pageNumber))
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setUsersTotalCountAC(totalCount))
-        }, 
-        toggleIsFetching: (isFetching) => {
-            dispatch(toggleIsFetchingAC(isFetching));
-        }
-    }
-}
+// let mapDispatchToProps = (dispatch) =>{
+//     return {
+//         follow: (userId) =>{
+//             dispatch(followAC(userId));
+//         },
+//         unfollow: (userId) =>{
+//             dispatch(unfollowAC(userId));
+//         },
+//         setUsers: (users) =>{
+//             dispatch(setUsersAC(users));
+//         },
+//         setCurrentPage: (pageNumber) => {
+//             dispatch(setCurrentPageAC(pageNumber))
+//         },
+//         setTotalUsersCount: (totalCount) => {
+//             dispatch(setUsersTotalCountAC(totalCount))
+//         }, 
+//         toggleIsFetching: (isFetching) => {
+//             dispatch(toggleIsFetchingAC(isFetching));
+//         }
+//     }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+export default connect(mapStateToProps, {
+    follow,
+    unfollow,
+    setUsers,
+    setCurrentPage,
+    setTotalUsersCount,
+    toggleIsFetching})(UsersContainer);
--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -2,12 +2,11 @@ import React from 'react';
 import preloader from "../../../assets/images/preloader.png";
 
 let Preloader = (props) => {
-    return <div style = {
-            { backgroundColor: 'white' } } >
-        <
-        img src = { preloader }
-    />  <
-    /div>
+    return <div  style={ {backgroundColor: 'white'} }>
+    <img src={preloader}/> 
+    </div>
 }
 
 export default Preloader;
+
+
