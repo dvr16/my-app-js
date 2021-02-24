05c7692817a3556453b6707183ebc868040cc1c1
Дмитрий <dvr07@yandex.ru>
2021-02-24 14:36:01 +0300

    adding cookie

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = () =>{
+const Header = (props) =>{
     return(
 <header className={s.header}>
             <img src='https://tekneloji.net/wp-content/uploads/2016/04/intel-300x300.png' />
+        
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
         </header>);
 }
 
